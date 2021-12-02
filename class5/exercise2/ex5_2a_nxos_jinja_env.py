fcdeceda2f2c89fba74f59dd67cf7d4878e2d6b8
boozinka <wbellward@gmail.com>
2021-12-02 08:46:50 +0000

    Basic Jinja2 templating with external template file

---
+++ class5/exercise2/ex5_2a_nxos_jinja_env.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+  
+# Import Jinja2 Environment
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+
+# Set undefined = strict to pick up errors is varibles
+env = Environment(undefined=StrictUndefined)
+# Set template file location (sub-directory of pwd)
+env.loader = FileSystemLoader('./templates')
+
+# Assign varibles
+nxos1_dict = {'interface': 'Ethernet1/1',
+             'ip_addr': '10.1.100.1',
+             'netmask': 24
+}
+nxos2_dict = {'interface': 'Ethernet1/1',
+             'ip_addr': '10.1.100.2',
+             'netmask': 24
+ }
+
+# Loop through each dictionary
+for var in (nxos1_dict, nxos2_dict):
+    template_file = 'ex5_2a_nxos_jinja_env.j2'
+    # Create template object
+    template = env.get_template(template_file)
+    # Render and assign to 'output' varible
+    output = template.render(**var)
+    print(output)
+
